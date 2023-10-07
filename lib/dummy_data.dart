0ae86fd089ea6cdc1c3f32419305e45b5ba9235d
Hasan Banna <hansolo132134@gmail.com>
2023-10-07 19:26:14 +0600

    modified and added new features

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -4,24 +4,32 @@ class DummyData {
   static List<Product> productsInCart = [
     Product(
         'Pullover',
-        'https://plus.unsplash.com/premium_photo-1673458070037-8cc98a465c0b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1355&q=80',
+        'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/454a5346-64ef-4467-8680-bd3ac29650c7/jordan-23-engineered-fleece-pullover-HJ7cbT.png',
         'Red',
-        'L'),
+        'L',
+        25.0,
+        1),
     Product(
         'T-Shirt',
-        'https://shop.smu.edu.sg/cdn/shop/products/smu-terry-pullover.jpg?v=1626438274',
+        'https://contents.mediadecathlon.com/p2014174/495fbfd5eabbd253be4ab8323e098e5a/p2014174.jpg',
         'Blue',
-        'M'),
+        'M',
+        30.0,
+        1),
     Product(
         'Spandex',
-        'https://shop.smu.edu.sg/cdn/shop/products/smu-terry-pullover.jpg?v=1626438274',
+        'https://upload.wikimedia.org/wikipedia/commons/b/b6/Cycling_kit_full_body_alt_3_%28cropped%29.jpg',
         'Green',
-        'XL'),
+        'XL',
+        20.0,
+        1),
     Product(
         'Jersey',
-        'https://shop.smu.edu.sg/cdn/shop/products/smu-terry-pullover.jpg?v=1626438274',
+        'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/645f3f29-56bf-415d-9697-c3492ee280a1/los-angeles-lakers-statement-edition-jordan-dri-fit-nba-swingman-jersey-lhdHs0.png',
         'Black',
-        'S'),
+        'S',
+        45.0,
+        1),
     // Add more products as needed
   ];
 }
@@ -31,6 +39,15 @@ class Product {
   final String imageUrl;
   final String color;
   final String size;
+  final double unitPrice;
+  int quantity; // Add the unit price property
 
-  Product(this.name, this.imageUrl, this.color, this.size);
+  Product(
+    this.name,
+    this.imageUrl,
+    this.color,
+    this.size,
+    this.unitPrice,
+    this.quantity,
+  );
 }
--- lib/shopping_cart.dart
+++ lib/shopping_cart.dart
@@ -2,14 +2,27 @@
 import 'package:flutter/material.dart';
 import 'search_bar.dart';
 import 'dummy_data.dart';
-import 'product_card.dart';
+import 'cart_item.dart';
 
-class ShoppingCart extends StatelessWidget {
+class ShoppingCart extends StatefulWidget {
+  @override
+  _ShoppingCartState createState() => _ShoppingCartState();
+}
+
+class _ShoppingCartState extends State<ShoppingCart> {
+  // Initially set the total amount based on the initial cart data
+  double totalAmount = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    updateTotalAmount(); // Calculate total amount initially
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(),
-      //backgroundColor: Colors.grey,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -18,7 +31,7 @@ class ShoppingCart extends StatelessWidget {
             child: Text(
               'My Bag',
               style: TextStyle(
-                fontSize: 24.0,
+                fontSize: 40.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
@@ -27,11 +40,86 @@ class ShoppingCart extends StatelessWidget {
             child: ListView.builder(
               itemCount: DummyData.productsInCart.length,
               itemBuilder: (context, index) {
-                return ProductCard(product: DummyData.productsInCart[index]);
+                return CartItem(
+                  product: DummyData.productsInCart[index],
+                  onQuantityChanged: updateTotalAmount,
+                );
               },
             ),
           ),
+          // Text and total amount above the "CHECK OUT" button
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Total amount:',
+                  style: TextStyle(fontSize: 18.0, color: Colors.grey),
+                ),
+                Text(
+                  '${totalAmount.toString()}\$',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // "CHECK OUT" button
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                showCongratulatorySnackbar(context);
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                shape: MaterialStateProperty.all<OutlinedBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                  EdgeInsets.symmetric(vertical: 15.0),
+                ),
+              ),
+              child: Container(
+                width: double.infinity,
+                child: Center(
+                  child: Text(
+                    'CHECK OUT',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  void updateTotalAmount() {
+    setState(() {
+      totalAmount = DummyData.productsInCart.fold(
+        0,
+        (previousValue, element) =>
+            previousValue + (element.unitPrice * element.quantity),
+      );
+    });
+  }
+
+  // Function to show the congratulatory snackbar
+  void showCongratulatorySnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Congratulations! Your order has been placed.'),
+        duration: Duration(seconds: 3),
       ),
     );
   }
